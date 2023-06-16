39f6503991610d70af229471ba3a66033a339fcd
lazma abderrahmane <abderahmane.lazma@gmail.com>
2023-06-16 22:43:55 +0000

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nb = 0;
+	while (nb < 10)
+	{
+		putchar(nb+'0');
+		nb++
+	}
+	putchar('\n');
+	return (0);
+}
+
