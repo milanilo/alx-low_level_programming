024d29a68c4feac155db6b71e878f6383b9b2e06
lazma abderrahmane <abderahmane.lazma@gmail.com>
2023-06-16 18:16:04 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nb = 0;
+
+	while (nb < 10)
+	{
+		printf("%d", nb);
+		nb++;
+	}
+	printf("\n");
+	return (0);
+}
+
