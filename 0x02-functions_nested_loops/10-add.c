d3df8fac9a365163d1290b1c9632b36b14f5ac0f
Lazma Abderrahmane <abderahmane.lazma@gmail.com>
2023-06-21 22:35:15 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * add - it add tow numbers
+ * @a: first number
+ * @b: seconde number
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
+
