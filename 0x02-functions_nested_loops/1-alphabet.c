95705a63904b6ab4dd3d6629c4aee974cf48213f
Lazma Abderrahmane <abderahmane.lazma@gmail.com>
2023-06-21 00:20:33 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include<sdtio.h>
+#include"main.h"
+/**
+ * print_alphabet - print the alphabet in lowercase
+ *
+ * Return: nothing because it is void
+ */
+void print_alphabet(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		_putchar(alph);
+		a++;
+	}
+}
+
