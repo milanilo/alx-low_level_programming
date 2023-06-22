30b12199c0f6cc4453b67ef9738d14e7280deb8b
Lazma Abderrahmane <abderahmane.lazma@gmail.com>
2023-06-22 23:21:06 +0000

    task 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * mul - calcule the multiplication of tow numbers
+ * @a: first number
+ * @b: seconde number
+ * Return: mult of a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
