18d6388816f7143f6489c265808d09b8b6d1446b
Lazma Abderrahmane <abderahmane.lazma@gmail.com>
2023-06-22 22:27:31 +0000

    task 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * _isupper - it checks a caracter is uppercase or not
+ * @c: caracter cheked
+ * Return: 1 if true and 0 if false
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (0);
+		_putchar('\n');
+	} else
+	{
+		return (0);
+	}
+}
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this folder will containe the list of task for the loops and functions part 2 in alx low programin
