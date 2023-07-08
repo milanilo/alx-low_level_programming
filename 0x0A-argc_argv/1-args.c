3b91a0459db2ba28ee256099f60faf84ed0c2990
Lazma Abderrahmane <abderahmane.lazma@gmail.com>
2023-07-08 00:57:43 +0000

    task 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 Success
+ * @argc: number of inputs
+ * @argv: inputs
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
+
