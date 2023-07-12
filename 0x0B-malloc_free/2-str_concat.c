6ada7270eaf5ef55f38b0c5bf1d228cf3318e0f6
Lazma Abderrahmane <abderahmane.lazma@gmail.com>
2023-07-12 11:18:45 +0000

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include<stdlib.h>
+#include"main.h"
+/**
+ * *str_concat - return a pointer for concatenated strings
+ *
+ * @s1: firest string
+ * @s2: seconde string
+ * Return: the concatenated strings
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	char *ptr;
+	unsigned int r = 0;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		s1[i] = s2[j];
+		j++;
+	}
+
+	unsigned int size = i + j + 1;
+
+	ptr = (char *)malloc(size * sizeof(char));
+	if (ptr == NULL || size < 0)
+	{
+		return (NULL);
+	}
+	while (r < size)
+	{
+		ptr[r] = s1[r];
+		r++;
+	}
+	return (ptr);
+}
+
