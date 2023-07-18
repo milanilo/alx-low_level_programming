acd87216db4e93329fdec15d8fad4ccc2efdd386
Lazma Abderrahmane <abderahmane.lazma@gmail.com>
2023-07-18 13:58:56 +0000

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,4 +13,12 @@ typedef struct dog
 	char *owner;
 	float age;
 } dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
